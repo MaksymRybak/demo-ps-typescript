83c4cf593696543f23fe73cf817b6798b33edeba
Maksym Rybak <rybak.maksym@gmail.com>
2020-12-04 17:25:26 +0100

    use webpack to compile ts and run the app

--- app/app.js
+++
@@ -1,6 +0,0 @@
-function startGame() {
-    // starting a new game
-    var messageElemenet = document.getElementById('messages');
-    messageElemenet.innerText = 'Welcome to MultiMath! Starting a new game...';
-}
-document.getElementById('startGame').addEventListener('click', startGame);
