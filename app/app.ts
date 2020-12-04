065868f7c5b72b5bdb28967199f151591f4db745
Maksym Rybak <rybak.maksym@gmail.com>
2020-12-04 17:59:32 +0100

    test strictNullChecks option of tsc

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,15 @@
 function startGame() {
   // starting a new game
+
+  let playerName: String = 'Audrey';
+  logPlayer(playerName);
+
   var messageElemenet = document.getElementById('messages');
   messageElemenet!.innerText = 'Welcome to MultiMath! Starting a new game...';
+}
+
+function logPlayer(name) {
+  console.log(`New game starting for player ${name}.`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
