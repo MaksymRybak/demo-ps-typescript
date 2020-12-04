eb3838b3a4353ea0167043016d1846ef5d505a70
Maksym Rybak <rybak.maksym@gmail.com>
2020-12-04 18:42:38 +0100

    examples of function types, default values, type annotation and casting

--- app/app.ts
+++ app/app.ts
@@ -1,15 +1,48 @@
 function startGame() {
   // starting a new game
 
-  let playerName: String = 'Audrey';
+  let playerName: string | undefined = getInputValue('playername');
   logPlayer(playerName);
 
-  var messageElemenet = document.getElementById('messages');
-  messageElemenet!.innerText = 'Welcome to MultiMath! Starting a new game...';
+  postScore(80, playerName);
+  postScore(-50, playerName);
 }
 
-function logPlayer(name) {
+function logPlayer(name: string = 'MultiMath Player'): void {
   console.log(`New game starting for player ${name}.`);
 }
 
+function getInputValue(elementID: string): string | undefined {
+  const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+
+  if (inputElement.value === '') {
+    return undefined;
+  } else {
+    return inputElement.value;
+  }
+}
+
+function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+  let logger: (value: string) => void;
+
+  if (score < 0) {
+    logger = logError;
+  } else {
+    logger = logMessage;
+  }
+
+  const scoreElement: HTMLElement | null = document.getElementById('postedScores');
+  scoreElement!.innerText = `${score} ${playerName}`;
+
+  logger(`Score: ${score}`);
+}
+
 document.getElementById('startGame')!.addEventListener('click', startGame);
+
+const logMessage = (message: string) => console.log(message);
+
+logMessage('Welcome to MultiMath!');
+
+function logError(err: string): void {
+  console.error(err);
+}
